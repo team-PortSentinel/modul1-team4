6488acb9335f369cb9f72617621602b04255802f
YoonSunWoo02 <xha020215@mgail.com>
2026-07-14 13:07:06 +0900

    X데이터에 cwe 칼럼 추가

--- 3TEAM/feature.py
+++ 3TEAM/feature.py
@@ -7,7 +7,7 @@ print(f" 원본 데이터 로드: {len(df)}행")
 
 print(df.shape)
 
-feature_cols = ['attack_vector', 'attack_complexity','privileges_required', 'user_interaction']
+feature_cols = ['attack_vector', 'attack_complexity','privileges_required', 'user_interaction','cwe']
 
 # X: 범주형 피처 4개를 원-핫 인코딩
 df_encoded = pd.get_dummies(df, columns=feature_cols)
@@ -18,14 +18,11 @@ lable = LabelEncoder()
 df_encoded['severity_label'] = lable.fit_transform(df['severity'])
 
 # X에서 학습할 칼럼들만 남기고 제거하기
-feature_prefixes = ['attack_vector_', 'attack_complexity_',
-                     'privileges_required_', 'user_interaction_']
-
 train_cols = []
 
 #인코딩 하여 기존의 칼럼 이름을 사용할 수 없음 -> 접두어만 사용해서 비교
 for col in df_encoded.columns:
-    for prefix in feature_prefixes:
+    for prefix in feature_cols:
         if col.startswith(prefix):
             train_cols.append(col)
             break
