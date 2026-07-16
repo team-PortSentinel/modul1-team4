d13be4d903e748451d53ae84e654d3aa993bd36c
Yeop <an4158@naver.com>
2026-07-16 14:26:54 +0900

    코드 정리

--- app.py
+++ app.py
@@ -29,15 +29,25 @@ from utils.scanner import run_scanner
 # team2 폴더 안의 app_bridge를 통해 팀2 분석 기능을 하나의 함수로 사용
 from team2.app_bridge import run_vulnerability_scan
 
-# 팀2 변수 지정 (일단 team2_result로 해놨고, team1_result만 받아서 사용하면 됨)
-# team2_result = run_vulnerability_scan(team1_result) 
+#### 각 팀들 모듈 연결 (팀1 > 팀2 > 팀3 > 팀4)
 
-
-
-# 1) Live Nmap 실시간 스캔 가동 처리 (scanner.py 결과 활용)
+# 팀1 Live Nmap 실시간 스캔 가동 처리 (scanner.py 결과 활용)
 # target_input : ip 혹은 도메인 입력값
 # scan_options : 검색 옵션 입력값
 if start_live_scan:
     with st.spinner(f"🔍 '{target_input}'을(를) 대상으로 Nmap 실시간 정밀 스캔 가동 중..."):
         raw_output = run_scanner(target_input, scan_options)
     st.rerun()
+
+# 팀2 변수 지정 (일단 team2_result로 해놨고, team1_result만 받아서 사용)
+team2_result = run_vulnerability_scan(raw_output) 
+
+# 팀3 모델 
+
+# 팀4 
+
+
+
+### 화면 구현 
+
+
